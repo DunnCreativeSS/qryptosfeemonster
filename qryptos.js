c50fdb0c0abc41356994956bc1acbeaeb18a1ee5
Jarett Dunn <jarettrsdunn@gmail.com>
2018-06-26 20:49:32 -0300

    order limit

--- qryptos.js
+++ qryptos.js
@@ -333,6 +333,7 @@ async function dodatthing(qryptos, lpairs, pairs, balances) {
 					for (var i in orders) {
 						
 					if (orders[i].status == 'open'){
+						orders[i].fee = math.format(orders[i].fee.cost,{exponential:{lower:1e-100,upper:1e100}})
 						orders3.push(orders[i]);
 					}
 					
@@ -353,6 +354,7 @@ async function dodatthing(qryptos, lpairs, pairs, balances) {
 			orders3.sort(sortFunction);
 			orders5.sort(sortFunction);
 			for (var i in orders5){
+				orders5[i].fee = math.format(orders5[i].fee.cost,{exponential:{lower:1e-100,upper:1e100}})
 				if (oCount <= 15){
 				oCount++;
 				orders4.push(orders5[i]);
