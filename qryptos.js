6157b68787268e9a1a300aeeffa456050f31d895
DunnCreativeSS <jarett+github@dcssquared.com>
2018-06-26 19:18:20 -0300

    apikey

--- qryptos.js
+++ qryptos.js
@@ -41,9 +41,9 @@ app.get('/', function(req, res) {
 				+ '<br>trades last hr: ' + hrCount 
 				+ '<br>trades last 24hr: ' + moreHrCount 
 				+ '<br>avg. trades/hr last 24hr: ' + (moreHrCount / 24).toFixed(2)
-				+ '<br><br>fees last hr: ' + math.format(feesHr,{exponential:{"lowerExp":-100,"upperExp":100}})
-				+ '<br>fees last 24hr: ' + math.format(feesMoreHr ,{exponential:{"lowerExp":-100,"upperExp":100}})
-				+ '<br>avg. fees/hr last 24hr: ' + math.format((feesMoreHr / 24),{exponential:{"lowerExp":-100,"upperExp":100}})
+				+ '<br><br>fees last hr: ' + math.format(feesHr,{exponential:{lower:1e-100,upper:1e100}})
+				+ '<br>fees last 24hr: ' + math.format(feesMoreHr ,{exponential:{lower:1e-100,upper:1e100}})
+				+ '<br>avg. fees/hr last 24hr: ' + math.format((feesMoreHr / 24),{exponential:{lower:1e-100,upper:1e100}})
 				
 				+ '<br><br>percent/hr: <h1>' + percentHr + '%</h1>'
 				+ '<br><br>current, open orders: <br><div style="display:none;" id="orders">' + JSON.stringify(orders3) + '</div><div style="display:none;" id="orders4">' + JSON.stringify(orders4) + '</div><div id="showData"></div><br><br>filled orders: <br><div id="showData2"></div><script> for(var col=[],i=0;i<JSON.parse($("#orders").text()).length;i++)for(var key in JSON.parse($("#orders").text())[i])-1===col.indexOf(key)&&col.push(key);var table=document.createElement("table"),tr=table.insertRow(-1);for(i=0;i<col.length;i++){var th=document.createElement("th");th.innerHTML=col[i],tr.appendChild(th)}for(i=0;i<JSON.parse($("#orders").text()).length;i++){tr=table.insertRow(-1);for(var j=0;j<col.length;j++){var tabCell=tr.insertCell(-1);tabCell.innerHTML=JSON.parse($("#orders").text())[i][col[j]]}}var divContainer=document.getElementById("showData");divContainer.innerHTML="",divContainer.appendChild(table);for(var col=[],i=0;i<JSON.parse($("#orders4").text()).length;i++)for(var key in JSON.parse($("#orders4").text())[i])-1===col.indexOf(key)&&col.push(key);var table2=document.createElement("table"),tr=table2.insertRow(-1);for(i=0;i<col.length;i++){var th=document.createElement("th");th.innerHTML=col[i],tr.appendChild(th)}for(i=0;i<JSON.parse($("#orders4").text()).length;i++){tr=table2.insertRow(-1);for(var j=0;j<col.length;j++){var tabCell=tr.insertCell(-1);tabCell.innerHTML=JSON.parse($("#orders4").text())[i][col[j]]}}var divContainer2=document.getElementById("showData2");divContainer2.innerHTML="",divContainer2.appendChild(table2);</script>');
@@ -287,7 +287,7 @@ async function dodatthing(qryptos, lpairs, pairs, balances) {
 						
 
 						orders4.push(orders[i]);
-						console.log(math.format(orders[i].fee.cost,{exponential:{"lowerExp":-100,"upperExp":100}}));
+						//console.log(math.format(orders[i].fee.cost,{exponential:{lower:1e-100,upper:1e100}}));
 
 						console.log(Number());
 						var diff3 = Math.abs(new Date() - new Date(orders[i].timestamp));
