a5909641fbeb5ca7444809d5e95118ccda52e9e2
Jarett Dunn <jarettrsdunn@gmail.com>
2018-06-27 16:27:52 -0300

    gobuy

--- qryptos.js
+++ qryptos.js
@@ -919,7 +919,7 @@ function doOrders2(pairs, lp, p, qryptos, balances, orders2, total) {
                         }
 								}
                     }
-                    if (bought <= 0 && sold <= 1) {
+                    if (bought <= 0 && sold <= 1 && gobuy == true) {
 						gobuy = false;
 								if (sList.includes(lp.pair)){
 									price = lp.ob.bid2.price * bidrate;
@@ -940,7 +940,9 @@ function doOrders2(pairs, lp, p, qryptos, balances, orders2, total) {
 								}
 								else {
                         price = lp.ob.bid.price * bidrate;
-                        if ((balances.BTC.free / price > lp.minimum && balances[lp.which].free <= lp.minimum && bought <= 0 && sold <= 1)) { // && (balances[lp.which].free <= lp.minimum)){//hardwire btc/eth
+                        if ((balances.BTC.free / price > lp.minimum && balances[lp.which].free <= lp.minimum && bought <= 0 && sold <= 1 && gobuy == true)) {
+gobuy = false;
+							// && (balances[lp.which].free <= lp.minimum)){//hardwire btc/eth
                             ////console.log('buying 1');
                             ////console.log(lp.ob.bid.price * bidrate);
                             try {
