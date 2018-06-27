2458bf6ef5002d714ad87e6f7d856c0b5b3a2fd6
Jarett Dunn <jarettrsdunn@gmail.com>
2018-06-27 01:57:32 -0300

    ccxt

--- qryptos.js
+++ qryptos.js
@@ -75,9 +75,9 @@ async function doOrders(lp, side, op, precision, price, qryptos, balance, callba
 			balance = balances.BTC.free;
 			balance2 = balance;
 			console.log('buy init bal ' + balance);
-				if ((balance / 2.05/  price * .995).toFixed(8) > (lp.minimum * 1.2)) {
+				if ((balance / 4.05/  price * .995).toFixed(8) > (lp.minimum * 1.2)) {
 			
-				balance = (balance2 / 2.05/  price * .995).toFixed(8);
+				balance = (balance2 / 4.05/  price * .995).toFixed(8);
 				
 			
 				} else {
